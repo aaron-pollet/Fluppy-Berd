a74a2f5c4c8963fedb2fe57a427d7c43850f1dd3
aaronpollet <pollet_aaron@hotmail.com>
2024-08-28 17:29:29 +0200

    game over screen
    still need to add logic to game over when outside screen, add a highscore, Update way of taking Input (Input System in Package Manager), Update Text system with Text Mesh Pro, add background with clouds, title screen, sound effects, animation of the wings, etc

--- Fluppy Berd/Assets/BirdScript.cs
+++ Fluppy Berd/Assets/BirdScript.cs
@@ -1,20 +1,29 @@
+using System;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
     public float flapStrength;
+    public LogicScript logic;
+
+    public bool birdIsAlive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
             rigidBody.velocity = Vector2.up * flapStrength;
     }
-          
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.GameOver();
+        birdIsAlive = false;
+    }
 }
